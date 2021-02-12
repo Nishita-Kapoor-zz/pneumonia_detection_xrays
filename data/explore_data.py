26e40ddbfbb9f7b6de0696d9b72c8ca6b09b3f76
Nishita Kapoor <nishita.s66@gmail.com>
2021-02-12 18:47:16 +0100

    Added EDA first implementation

---
+++ data/explore_data.py
@@ -0,0 +1,100 @@
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+def create_plots(**cfg):
+
+    cat_df, image_df = create_csv(cfg["datadir"])
+
+    if not os.path.exists("./output/data_analysis/plots/"):
+        os.makedirs("./output/data_analysis/plots/")
+
+    # Plot 1: Count of categories
+    cat_df.set_index('category')['n_train'].plot.bar(
+        color='c', figsize=(20, 6))
+    plt.xticks(rotation=0)
+    plt.ylabel('Count')
+    plt.title('Training Images by Category')
+
+    plt.savefig("./output/data_analysis/plots/train_category.png", dpi=300)
+
+    # Plot 2: Distribution Plot - Average Sizes
+    img_dsc = image_df.groupby('category').mean()
+    plt.figure(figsize=(10, 6))
+    sns.kdeplot(
+        img_dsc['height'], label='Average Height')
+    sns.kdeplot(
+        img_dsc['width'], label='Average Width')
+    plt.xlabel('Pixels')
+    plt.ylabel('Density')
+    plt.title('Average Size Distribution')
+    plt.savefig("./output/data_analysis/plots/average_size.png", dpi=300)
+
+    print("Both plots created and saved in ./output/data_analysis/plots/")
+
+def create_csv(datadir):
+
+    traindir = datadir + "/train/"
+    validdir = datadir + "/val/"
+    testdir = datadir + "/test/"
+
+    if not os.path.exists("./output/data_analysis/"):
+        os.makedirs("./output/data_analysis/")
+
+    # Iterate through each category
+    if os.path.exists("./output/data_analysis/tables/"):
+        cat_df = pd.read_csv("./output/data_analysis/tables/cat_df.csv")
+        image_df = pd.read_csv("./output/data_analysis/tables/image_df.csv")
+    else:
+        # Create a folder to save csv files
+        os.makedirs("./output/data_analysis/tables/")
+
+        # Initialize lists
+        categories = []
+        img_categories = []
+        n_train = []
+        n_valid = []
+        n_test = []
+        hs = []
+        ws = []
+
+        # Read all images and save dataframes to disk
+        for d in os.listdir(traindir):
+            if not d.startswith('.'):
+                categories.append(d)
+                # Number of each image
+                train_imgs = os.listdir(traindir + d)
+                valid_imgs = os.listdir(validdir + d)
+                test_imgs = os.listdir(testdir + d)
+                n_train.append(len(train_imgs))
+                n_valid.append(len(valid_imgs))
+                n_test.append(len(test_imgs))
+                # Find stats for train images
+                for i in train_imgs:
+                    if not i.startswith('.'):
+                        img_categories.append(d)
+                        img = Image.open(traindir + d + '/' + i)
+                        img_array = np.array(img)
+                        # Shape
+                        hs.append(img_array.shape[0])
+                        ws.append(img_array.shape[1])
+
+        # Dataframe of categories
+        cat_df = pd.DataFrame({'category': categories,
+                               'n_train': n_train,
+                               'n_valid': n_valid, 'n_test': n_test}). \
+            sort_values('category')
+        # Dataframe of training images
+        image_df = pd.DataFrame({
+            'category': img_categories,
+            'height': hs,
+            'width': ws
+        })
+        cat_df.to_csv("./output/data_analysis/tables/cat_df.csv")
+        image_df.to_csv("./output/data_analysis/tables/image_df.csv")
+
+    return cat_df, image_df
+
---
+++ main.py
@@ -0,0 +1,38 @@
+# Main file to run
+# Author: Nishita Kapoor
+# Functionality:
+
+# Package Imports
+import yaml
+import argparse
+from data.explore_data import create_plots
+
+# Main function
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", help="Path of config file, e.g. configs/config.yaml", default="configs/config.yaml",
+                        type=str)
+    args = parser.parse_args()
+
+    # Read config file
+    with open(str(args.config), "r") as ymlfile:
+        cfg = yaml.load(ymlfile)
+        print(cfg)
+
+    # Step 1: EDA
+    if cfg["plot"]:
+        create_plots(**cfg)
+
+    # Step 2: Pre-processing
+
+    # Step 3: Training the model
+
+    # Step 4: Prediction
+
+    # Step 5: Visualization
+
+
+# How to run:
+# python main.py --config configs/config.yaml
+if __name__ == '__main__':
+    main()
